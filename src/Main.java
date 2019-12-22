63c7d139fdf0c47ad212859e7dd006d6094d3a9c
PavelMarusov <pavelmarusovs@gmail.com>
2019-12-22 17:08:47 +0600

    COMMIT

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,40 @@
 import java.util.*;
-import java.util.concurrent.Callable;
 
 public class Main {
 
     public static void main(String[] args) {
         ArrayList<String> a = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        System.out.println("5");
         a.add(scanner.next());
+        System.out.println("4");
         a.add(scanner.next());
+        System.out.println("3");
         a.add(scanner.next());
+        System.out.println("2");
         a.add(scanner.next());
+        System.out.println("1");
         a.add(scanner.next());
+        System.out.println("0");
         System.out.println("Распечатай коллекцию А");
         Iterator iterator = a.iterator();
         while (iterator.hasNext()) {
             Object d = iterator.next();
-            System.out.println(d);
+            System.out.println(d );
 
         }
         ArrayList<String> b = new ArrayList<>();
+
         b.add(scanner.next());
+
         b.add(scanner.next());
+
         b.add(scanner.next());
+
         b.add(scanner.next());
+
         b.add(scanner.next());
+
         System.out.println("Распечатай коллекцию B ");
 
         Collections.sort(b);
@@ -55,40 +66,22 @@ public class Main {
             System.out.println(o);
 
         }
-        Collections.sort(c);
+        System.out.println("Распечатай отсортированый список");
+        Collections.sort(c, new Comparator<String>() {
 
-        System.out.println("Отсортированный список С");
-
+            public int compare(String o, String o1) {
+                return o.length() - o1.length();
+            }
+        });
         iterator2 = c.iterator();
         while (iterator2.hasNext()) {
-            Object s = iterator2.next();
-            System.out.println(s);
+            System.out.println(iterator2.next());
         }
-
-//        System.out.println("_________________________");
-//        for (int i = 0; i <c.size() ; i+=2) {
-//            int p =1;
-//            p+=2;
-//        compareTo(c.get(i),c.get(p));
-//
-//        }
-
 
 
     }
 
 
-//    public static  int compareTo(String o, String a) {
-//        if(o.length()>a.length()){
-//
-//           System.out.println(o);
-    //           return -1;
-    //       }else if(o.length()<a.length()){
-//            System.out.println(a);
-    //           return 1;
-    //     }
-    //       return 0;
-    //   }
 }
 
 
